dd60ed3cd633c809282e8fa36449689746c63095
Stefano Kowalke <info@arroba-it.de>
2022-01-28 16:39:27 +0100

    Add Metaschema for draft 7

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const addFormats = require('ajv-formats');
 const Ajv2020 = require('ajv/dist/2020')
 const ajv = new Ajv2020({ strict: false, allErrors: true })
 require("ajv-keywords")(ajv)
+const draft7MetaSchema = require("ajv/dist/refs/json-schema-draft-07.json")
+ajv.addMetaSchema(draft7MetaSchema)
 addFormats(ajv);
 
 let schema = JSON.parse(fs.readFileSync('statamic.blueprint.schema.json', 'utf8'));
